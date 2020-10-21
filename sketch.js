2c92a29aa81e95c08b908a6b663d7a502cf3448f
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2020-10-20 22:15:56 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,16 +26,16 @@ function setup() {
 	World.add(world, mConstraint);
 
 	//Create the Bodies Here.
-	pendulum1 = new Pendulum(windowWidth-450, windowHeight - 400, "grey");
-	sling1 = new Sling(pendulum1.body,{x:windowWidth-450, y:windowHeight - 400});
-	pendulum2 = new Pendulum(windowWidth-510, windowHeight - 400, "grey");
-	sling2 = new Sling(pendulum2.body,{x:windowWidth-510, y:windowHeight - 400});
-	pendulum3 = new Pendulum(windowWidth-570, windowHeight - 400, "grey");
-	sling3 = new Sling(pendulum3.body,{x:windowWidth-570, y:windowHeight - 400});
-	pendulum4 = new Pendulum(windowWidth-630, windowHeight - 400, "grey");
-	sling4 = new Sling(pendulum4.body,{x:windowWidth-630, y:windowHeight - 400});
-	pendulum5 = new Pendulum(windowWidth-690, windowHeight - 400, "grey");
-	sling5 = new Sling(pendulum5.body,{x:windowWidth-690, y:windowHeight - 400});
+	pendulum1 = new Pendulum(windowWidth-480, windowHeight - 400, "grey");
+	sling1 = new Sling(pendulum1.body,{x:windowWidth-480, y:windowHeight - 400});
+	pendulum2 = new Pendulum(windowWidth-540, windowHeight - 400, "grey");
+	sling2 = new Sling(pendulum2.body,{x:windowWidth-540, y:windowHeight - 400});
+	pendulum3 = new Pendulum(windowWidth-600, windowHeight - 400, "grey");
+	sling3 = new Sling(pendulum3.body,{x:windowWidth-600, y:windowHeight - 400});
+	pendulum4 = new Pendulum(windowWidth-660, windowHeight - 400, "grey");
+	sling4 = new Sling(pendulum4.body,{x:windowWidth-660, y:windowHeight - 400});
+	pendulum5 = new Pendulum(windowWidth-720, windowHeight - 400, "grey");
+	sling5 = new Sling(pendulum5.body,{x:windowWidth-720, y:windowHeight - 400});
 	
 
 	Engine.run(engine);
